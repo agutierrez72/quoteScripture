5e1ecb83e1261f126d7128faab932a707e6d476d
agutierrez72 <agutierrez72@ucmerced.edu>
2021-04-06 20:00:21 -0700

    add a sample verse into list

--- memVerse.py
+++ memVerse.py
@@ -1,5 +1,3 @@
-
-
 #Class for Scripture: contains book, address, and text of each verse
 class Memory_Verse:
 	def __init__(self, book, address, verse):
@@ -13,3 +11,20 @@ class Memory_Verse:
 		print(txt.format(self.address[0], self.address[1]))
 		print(self.verse)
 		print
+
+def start():
+    #first verse added to Scripture Library
+	book = "Colossians"
+	address = [3, 12]
+	verse = "Therefore, as the elect of God holy and beloved, put on tender mercies, kindness, meekness, and long suffering."
+
+	#verse1 = Memory_Verse(book, address, verse)
+	#verse1.myfunc()
+
+	list = []
+	list.append(Memory_Verse(book, address, verse))
+	list[0].myfunc()
+
+	return list
+
+start()
